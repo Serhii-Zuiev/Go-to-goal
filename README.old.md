cfb5524fb8293f840b372938c7817a88083a2c3f
Serhii-Zuiev <walter94walter@gmail.com>
2020-06-16 19:50:52 +0300

    innitial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Go-to-goal
+GoIT bootcamp-18 final team project.
---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./app.module.css";
+
+function App() {
+  return <div className={styles.appContainer}>Privet!!!</div>;
+}
+
+export default App;
