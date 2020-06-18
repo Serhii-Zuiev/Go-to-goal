7a2052dd24a250606367f5a458a32ae12685cd45
Andreiev <xsergeix@ukr.net>
2020-06-18 13:29:27 +0300

    router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./app.module.css";
 import TasksPage from "./pages/tasks-page/TasksPage";
+import {router} from '../routes/router'
 
 function App() {
+  // const token = useSelector((state) => state.user.userToken);
+  const routing = router(false);
   return(
   <>
     <div className={styles.appContainer}>Privet!!!</div>
     <TasksPage/>
+    {routing}
   </>
   )
 }
