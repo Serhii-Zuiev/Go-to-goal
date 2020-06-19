097219f2c9631ab41cd0904d753f2f22bb6e098f
Andreiev <xsergeix@ukr.net>
2020-06-19 14:59:42 +0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,14 @@ import React from "react";
 import styles from "./app.module.css";
 import { router } from "../routes/router";
 import{TestRedux} from '../redux/textRedux'
-import AuthPage from './pages/auth-page/AuthPage'
-import TasksPage from './pages/tasks-page/TasksPage'
+
 
 function App() {
 
   const routing = router(false);
   return (
     <>
-      <div className={styles.appContainer}>
-        <AuthPage />
-       <TestRedux/>
-      </div>
-      <TasksPage />
-    
-      {routing}
+    <TestRedux/>
       <div className={styles.appContainer}>{routing}</div>
     </>
   );
