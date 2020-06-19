a65cf40de861d1633fed4bf5958b0fe50fb752e3
Serhii-Zuiev <walter94walter@gmail.com>
2020-06-19 17:15:26 +0300

    done auth page greeting section

--- src/components/pages/auth-page/AuthPage.js
+++ src/components/pages/auth-page/AuthPage.js
@@ -3,11 +3,14 @@ import { Route, Switch, withRouter } from "react-router-dom";
 import Header from "../../header/Header";
 import RegisterForm from "./AuthModal/RegisterForm/RegisterForm";
 import AvatarPicker from "./AuthModal/AvatarPicker/AvatarPicker";
+import Greeting from "./Greeting/Greeting";
+
+
 const AuthPage = () => {
   return (
     <>
       <Header />
-
+      <Greeting />
       <Switch>
         <Route path="/auth/register" component={RegisterForm} />
         <Route path="/auth/login" component={AvatarPicker} />
---
+++ src/components/pages/auth-page/Greeting/Greeting.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./greeting.module.css";
+
+const Greeting = () => {
+  let content = (
+    <>
+      <h2 className={styles.mainGreeting}>Вітаємо вас у нашій програмі!</h2>
+      <div className={styles.greetingContentWrapper}>
+        <span className={styles.greetingContent}>Вже є аккаунт?</span>
+        <NavLink to="/auth/login">
+          <span
+            className={`${styles.greetingContent} ${styles.greetingLoginLink}`}
+          >
+            Log in
+          </span>
+        </NavLink>
+      </div>
+      <NavLink to="/auth/register">
+        <button className={styles.toRegisterBtn}>Приєднатися!</button>
+      </NavLink>
+    </>
+  );
+
+  if (window.innerWidth > 767) {
+    content = (
+      <>
+        <div className={styles.greetingWrapper}>
+          <h2 className={styles.mainGreeting}>Вітаємо вас у нашій програмі!</h2>
+
+          <div className={styles.greetingContent}>
+            <p className={styles.greetingContentText}>
+              Тут ви зможете чітко сформулювати ваші бажання - чого ви хочете
+              досягти чи отримати у якості винагороди. Вам потрібно поставити
+              собі мету, а згодом - виконувати поставлені задачі у комфортному
+              для себе ритмі та не забувати відмічати їх виконання.
+            </p>
+            <p className={styles.greetingContentText}>
+              Будьте наполегливими, відповідальними і ви зможете досягти всього,
+              чого забажаєете! Подобається ідея? Готові допомагати, розвиватися
+              та йти до своєї мети?
+            </p>
+            <p className={styles.greetingContentText}>
+              Так, так, все саме так, як у дорослому житті :&#x232A; Тоді не
+              зважайте, а швидко зареєструйтеся, або заходьте на свій аккаунт.
+            </p>
+          </div>
+
+          <NavLink to="/auth/register">
+            <button className={styles.toRegisterBtn}>Приєднатися!</button>
+          </NavLink>
+        </div>
+      </>
+    );
+  }
+
+  return <header className={styles.header}>{content}</header>;
+};
+
+export default Greeting;
