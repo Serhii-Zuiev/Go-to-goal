b026a4f5c38d0ee5525a63490e93e340603e1d03
Andreiev <xsergeix@ukr.net>
2020-06-24 18:14:36 +0300

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { router } from "../routes/router";
+// import { TestRedux } from "../redux/textRedux";
 
 function App() {
   const token = useSelector((state) => state.userAuthReducer.userData.token);
   const routing = router(token);
   return (
     <>
+    {/* <TestRedux></TestRedux> */}
       {routing}
     </>
   );
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,48 +1,46 @@
 import React, { Suspense, lazy } from "react";
 import {
-  BrowserRouter as Router,
+
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
 import { LoaderUi } from "./loader/Loader";
 
-
 const AuthPage = lazy(() =>
   import(
-  "../components/pages/auth-page/AuthPage" /* webpackChunkName: 'AuthPage'*/
+    "../components/pages/auth-page/AuthPage" /* webpackChunkName: 'AuthPage'*/
   )
 );
-const TasksPage=lazy(()=>
-import(
-  "../components/pages/tasks-page/TasksPage" /* webpackChunkNAme: "TasksPage"*/
-))
+const TasksPage = lazy(() =>
+  import(
+    "../components/pages/tasks-page/TasksPage" /* webpackChunkNAme: "TasksPage"*/
+  )
+);
 export const router = (token) => {
   if (token) {
     return (
       
-      <Switch>
-        <Suspense fallback={<LoaderUi/>}>
-          <Route   path="/goals" component={'goals'} />
-          <Route  path="/tasks" component={TasksPage} />
-          <Route  path="/auth/logout" component={AuthPage} />
+      <Suspense fallback={<LoaderUi />}>
+        <Switch>
+          <Route exact path="/goals" component={"goals"} />
+          <Route exact path="/tasks" component={TasksPage} />
+          <Route path="/auth/logout" component={AuthPage} />
           <Redirect to="/goals" />
-      
-        </Suspense>
-  
-      </Switch>
+        </Switch>
+      </Suspense>
     );
   }
-  return (
-    <>
-      <Router>
-        <Suspense fallback={<LoaderUi/>}>
-          <Switch>
+  
+    return (
+      <>
+      
+          <Suspense fallback={<LoaderUi />}>
             <Route path="/" component={AuthPage} />
             <Redirect to="/" />
-          </Switch>
-        </Suspense>
-      </Router>
-    </>
-  );
+          </Suspense>
+   
+      </>
+    );
+  
 };
