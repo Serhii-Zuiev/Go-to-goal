1e11032625bcbb4cf5ee7472e0c16a49ae7b261c
Serhii-Zuiev <walter94walter@gmail.com>
2020-06-30 19:00:44 +0300

    auth modal + goal modal

--- src/components/pages/auth-page/AuthPage.js
+++ src/components/pages/auth-page/AuthPage.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Header from "../../header/Header";
 import RegisterForm from "./AuthModal/RegisterForm/RegisterForm";
-// import AvatarPicker from "./AuthModal/AvatarPicker/AvatarPicker";
 import Greeting from "./Greeting/Greeting";
 import Banner from "./Banner/Banner";
-
 
 const AuthPage = () => {
   return (
     <>
-    <Route path="/auth/register" component={RegisterForm} />
-      <Header pageOfHeader={'auth'}/>
-      <Banner/>
+      <Route path="/auth/register" component={RegisterForm} />
+      <Header pageOfHeader={"auth"} />
+      <Banner />
       <Greeting />
-     
     </>
   );
 };
 
-export default withRouter(AuthPage);
+export default AuthPage;
