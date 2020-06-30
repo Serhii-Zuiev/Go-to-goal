7c0e5c2167912ded6a85acd79806fdc3a20558b6
Andreiev <xsergeix@ukr.net>
2020-06-30 21:43:10 +0300

    new one

--- src/components/pages/tasks-page/congratulation/Congratulation.js
+++ src/components/pages/tasks-page/congratulation/Congratulation.js
@@ -2,13 +2,13 @@ import React from 'react';
 // eslint-disable-next-line
 import {conect} from "react-redux";
 import s from "./congratulation.module.css"
-const Congratulation = () => {
+const Congratulation = ({target}) => {
     // console.log(target)
     return(
         <div className={s.container}>
             <div className={s.modal}>
                 <h1 className={s.congrats}>Вітаємо</h1>
-                <p className={s.textInform}>Ти отримуєш</p>
+    <p className={s.textInform}>Ти отримуєш  {target}</p>
                 <div className={s.flagContainer}>
                     <p className={s.textPrise}>Ckjy</p>
                     <p className={s.textNewTask}>Час обрати нову ціль</p>
