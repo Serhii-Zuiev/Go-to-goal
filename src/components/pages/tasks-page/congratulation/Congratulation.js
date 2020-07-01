a0ff4e1b3688e68ac1766d15f21d34647c57b705
Serhii-Zuiev <walter94walter@gmail.com>
2020-07-01 08:57:15 +0300

    logic_for_progress_currunt_goal merged

--- src/components/pages/tasks-page/congratulation/Congratulation.js
+++ src/components/pages/tasks-page/congratulation/Congratulation.js
@@ -1,8 +1,6 @@
 import React from 'react';
-// eslint-disable-next-line
-import {conect} from "react-redux";
 import s from "./congratulation.module.css"
-import { Switch, Route, Redirect, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const Congratulation = ({target, token, goalOperation, tasks}) => {
     return(
         <div className={s.container}>
