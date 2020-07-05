8ec7f4eeb97480ab5648eeb0c491e1ff10775d81
Bohdan-C <bohdanchurik31@gmail.com>
2020-07-05 20:13:46 +0300

    fix controller components, and modal styles

--- src/components/pages/tasks-page/task-modal/TaskModal.js
+++ src/components/pages/tasks-page/task-modal/TaskModal.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import ModalBackDrop from '../../../modalBackDrop/ModalBackDrop'
+import ModalBackDrop from "../../../modalBackDrop/ModalBackDrop";
 import s from "./taskmodal.module.css";
 import Select from "react-select";
-// const keyforElement = shortid.generate();
 
 const IDS = {
   INPUT_VALUE: "title",
@@ -11,7 +10,7 @@ const IDS = {
 };
 
 const options = [
-  { value: "8.00-10", label: "8.00-10.00" },
+  { value: "8.00-10.00", label: "8.00-10.00" },
   { value: "10.00-12.00", label: "10.00-12.00" },
   { value: "12.00-14.00", label: "12.00-14.00" },
   { value: "14.00-16.00", label: "14.00-16.00" },
@@ -22,24 +21,26 @@ const options = [
 const findOpt = (value) => options.find((opt) => opt.value === value);
 
 function TaskModal(props) {
-  const handleFormforUsers = props.handleFormforUsers
-  const handleCloseModalWindow = props.handleCloseModalWindow
+  const handleFormforUsers = props.handleFormforUsers;
+  const handleCloseModalWindow = props.handleCloseModalWindow;
   const [state, setState] = useState({
     [IDS.INPUT_VALUE]: "",
     [IDS.INPUT_POINT]: "",
     [IDS.SELECT_DATA]: "",
   });
 
+  console.log("state", state);
+
   const handleForm = (e) => {
-    e.preventDefault()
-    const task ={
-      title: state['title'],
-      points: state['points'],
-      deadline: state['deadline'],
-    }
-    handleFormforUsers(task)
-    handleCloseModalWindow()
-  } 
+    e.preventDefault();
+    const task = {
+      title: state["title"],
+      points: state["points"],
+      deadline: state["deadline"],
+    };
+    handleFormforUsers(task);
+    handleCloseModalWindow();
+  };
 
   const handleChgange = (e) => {
     const { id, value } = e.target;
@@ -50,10 +51,17 @@ function TaskModal(props) {
   };
 
   const onChangeSelect = (opt) => {
-    setState({
+    setState((currentState) => ({
+      ...currentState,
       [IDS.SELECT_DATA]: opt.value,
-    });
+    }));
   };
+
+  // const onChangeSelect = (opt) => {
+  //   setState({
+  //     [IDS.SELECT_DATA]: opt.value,
+  //   });
+  // };
 
   return (
     <div className={s.modal_container}>
@@ -64,36 +72,39 @@ function TaskModal(props) {
           placeholder="(Оберіть завдання або додай нове)"
           maxLength="20"
           minLength="3"
+          name="title"
           id={IDS.INPUT_VALUE}
           value={state[IDS.INPUT_VALUE]}
           onChange={handleChgange}
           className={s.input_task}
           required
         />
-        <div className={s.input_options_section}>
-          <Select
-            placeholder="Час"
+        <div className={s.inputs_containers}>
+          <div className={s.input_options_section}>
+            <Select
+              placeholder="Час"
+              required
+              options={options}
+              onChange={onChangeSelect}
+              id={IDS.SELECT_DATA}
+              value={findOpt(state[IDS.SELECT_DATA])}
+              className={s.input_options}
+            />
+          </div>
+          <input
+            type="text"
+            placeholder="ВИНАГОРОДА (наприклад 1000)"
+            maxLength="20"
+            minLength="3"
+            max="1000"
+            min="1"
+            id={IDS.INPUT_POINT}
+            value={state[IDS.INPUT_POINT]}
+            onChange={handleChgange}
+            className={s.input_options_input}
             required
-            options={options}
-            onChange={onChangeSelect}
-            id={IDS.SELECT_DATA}
-            value={findOpt(state[IDS.SELECT_DATA])}
-            className={s.input_options}
           />
         </div>
-        <input
-          type="text"
-          placeholder="ВИНАГОРОДА (наприклад 1000)"
-          maxLength="20"
-          minLength="3"
-          max="1000"
-          min="1"
-          id={IDS.INPUT_POINT}
-          value={state[IDS.INPUT_POINT]}
-          onChange={handleChgange}
-          className={s.input_options_input}
-          required
-        />
         <button type="submit" className={s.button}>
           OK
         </button>
